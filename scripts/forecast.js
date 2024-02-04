f73125a8436167ff4558a3967b2d3ebbb75af274
taki199 <christotaki22@gmail.com>
2024-02-05 00:15:02 +0100

    i added some files

---
+++ scripts/forecast.js
@@ -0,0 +1,18 @@
+const key = 'rDAhUrMuHnyEZLhWMC6BZU9Kw5tVlWMA';
+// get weather 
+const  getweather = async(id)=>{
+    const base ='http://dataservice.accuweather.com/currentconditions/v1/'
+    const query=`${id}?apikey=${key}`; 
+    const response=await fetch(base+query)
+    const data=await response.json();
+   return(data[0]);
+};
+
+// get City infos
+const getCity = async(city)=>{
+    const base ='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`
+    const response = await fetch(base + query)
+    const data = await response.json();
+    return (data[0]);
+}
